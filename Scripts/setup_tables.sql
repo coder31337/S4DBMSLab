a4f6277b9edeb66162daec1e91caea16cfbd6fd0
coder31337 <coder31337@coder31337>
2024-03-16 16:16:05 +0000

    Add setup_tables.sql

---
+++ Scripts/setup_tables.sql
@@ -0,0 +1,75 @@
+-- This script creates the schema of the six tables
+-- EMPLOYEE, DEPARTMENT, DEPT_LOCATIONS, PROJECT, WORKS_ON and DEPENDENT
+
+
+-- Delete any existing tables
+
+-- For Oracle
+-- DROP TABLE IF EXISTS [table] CASCADE CONSTRAINTS;
+
+-- For MySQL
+-- SET FOREIGN_KEY_CHECKS = 0;
+-- DROP TABLE IF EXISTS EMPLOYEE, DEPARTMENT, DEPT_LOCATIONS, PROJECT, WORKS_ON, DEPENDENT;
+-- SET FOREIGN_KEY_CHECKS = 1;
+
+
+CREATE TABLE EMPLOYEE
+(
+    Fname VARCHAR(7) NOT NULL,
+    Minit CHAR,
+    Lname VARCHAR(15) NOT NULL,
+    Ssn CHAR(9) PRIMARY KEY,
+    Bdate DATE,
+    Address VARCHAR(30),
+    Sex CHAR,
+    Salary DECIMAL(10, 2),
+    Super_ssn CHAR(9),
+    Dno INT NOT NULL
+);
+
+CREATE TABLE DEPARTMENT
+(
+    Dname VARCHAR(15) NOT NULL,
+    Dnumber INT PRIMARY KEY,
+    Mgr_ssn CHAR(9) NOT NULL,
+    Mgr_start_date DATE,
+    FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn)
+);
+
+CREATE TABLE DEPT_LOCATIONS
+(
+    Dnumber INT NOT NULL,
+    Dlocation VARCHAR(15) NOT NULL,
+    PRIMARY KEY (Dnumber, Dlocation),
+    FOREIGN KEY (Dnumber) REFERENCES DEPARTMENT(Dnumber)
+);
+
+CREATE TABLE PROJECT
+(
+    Pname VARCHAR(15) NOT NULL UNIQUE,
+    Pnumber INT NOT NULL PRIMARY KEY,
+    Plocation VARCHAR(15),
+    Dnum INT NOT NULL,
+    FOREIGN KEY (Dnum) REFERENCES DEPARTMENT(Dnumber)
+);
+
+CREATE TABLE WORKS_ON
+(
+    Essn CHAR(9) NOT NULL,
+    Pno INT NOT NULL,
+    Hours DECIMAL(3, 1) NOT NULL,
+    PRIMARY KEY (Essn, Pno),
+    FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
+    FOREIGN KEY (Pno) REFERENCES PROJECT(Pnumber)
+);
+
+CREATE TABLE DEPENDENT
+(
+    Essn CHAR(9) NOT NULL,
+    Dependent_name VARCHAR(15) NOT NULL,
+    Sex CHAR,
+    Bdate DATE NOT NULL,
+    Relationship VARCHAR(8),
+    PRIMARY KEY (Essn, Dependent_name),
+    FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn)
+);
